78413ce594138d91b2a04a09b98e0c58676557b9
Stanley <stanley.nguyen@sjsu.edu>
2023-03-09 12:30:52 -0800

    version 0

---
+++ src/mvc/AppFactory.java
@@ -0,0 +1,17 @@
+package mvc;
+
+/*
+Edits:
+    Stanley 3/8/23: created file
+    Stanley 3/8/23: completed file
+*/
+
+public interface AppFactory {
+    public Model makeModel();
+    public View makeView();
+    public String getTitle();
+    public String[] getHelp();
+    public String about();
+    public String[] getEditCommands();
+    public Command makeEditCommand(String s);
+}
