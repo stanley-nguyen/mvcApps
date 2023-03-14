602c0c7ed64b80fb7fe6521845f1ea1adee86c82
Bryant Bautista <77865786+bryantbautista@users.noreply.github.com>
2023-03-14 16:03:22 -0700

    Update Command.java

--- src/mvc/Command.java
+++ src/mvc/Command.java
@@ -7,8 +7,11 @@ Edits:
 
 public abstract class Command {
     protected Model model;
+    
     public Command (Model model) {
         this.model = model;
     }
+    
+    // execute() method will be implemented in the ChangeCommand class (in the Minefield package)
     public abstract void execute();
 }
