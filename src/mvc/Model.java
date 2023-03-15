b4ae36c1d76c67c8d86fe54d8cc18e90377d5685
Sanjana Jagarlapudi <sanjanajag123@gmail.com>
2023-03-15 09:52:20 -0700

    Created instance variables and constructor for Model class

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -4,5 +4,12 @@ Edits:
     Sanjana 3/14/23: created file
 
 */
-public class Model {
+public class Model extends Bean{
+    Boolean unsavedChanges;
+    String fileName;
+
+    public Model(Boolean unsavedChanges, String fileName){
+        this.unsavedChanges = unsavedChanges;
+        this.fileName = fileName;
+    }
 }
