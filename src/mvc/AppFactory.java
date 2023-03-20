7549411005332c18d526e877af1f56ce8a8fab0e
Sanjana Jagarlapudi <sanjanajag123@gmail.com>
2023-03-20 16:57:32 -0700

    updating makeEditCommand()

--- src/mvc/AppFactory.java
+++ src/mvc/AppFactory.java
@@ -4,15 +4,15 @@ package mvc;
 Edits:
     Stanley 3/8/23: created file
     Stanley 3/8/23: completed file
-    Stanley 3/16/23: updated makeView() and makeEditCommand() with new parameters
 */
+
 
 public interface AppFactory {
     public Model makeModel();
-    public View makeView(Model m);
+    public View makeView();
     public String getTitle();
     public String[] getHelp();
     public String about();
     public String[] getEditCommands();
-    public Command makeEditCommand(Model m, String s, Object o);
+    public Command makeEditCommand(String s);
 }
