9a518b8f54b33f77afc5cc3cbb2abb8f8f7e741b
Sanjana Jagarlapudi <sanjanajag123@gmail.com>
2023-03-20 17:19:32 -0700

    updating git

---
+++ src/MineField/MineFieldPanel.java
@@ -0,0 +1,67 @@
+package minefield;
+import mvc.*;
+/**
+ * Edits:
+ * Sanjana 3/18/23: Created MineFieldPanel Class.
+ * Sanjana 3/18/23: Wrote Constructor
+ * Sanjana 3/18/23: wrote main method.
+ */
+import javax.swing.*;
+
+public class MineFieldPanel extends AppPanel {
+    private JButton N; //change
+    private JButton NE;
+    private JButton E;
+    private JButton SE;
+    private JButton S;
+    private JButton SW;
+    private JButton W;
+    private JButton NW;
+
+
+    public MineFieldPanel(AppFactory factory) {
+        super(factory);
+        N = new JButton("N");
+        N.addActionListener(this);
+        controlPanel.add(N);
+
+        NE = new JButton("NE");
+        NE.addActionListener(this);
+        controlPanel.add(NE);
+
+        E = new JButton("E");
+        E.addActionListener(this);
+        controlPanel.add(E);
+
+        SE = new JButton("SE");
+        SE.addActionListener(this);
+        controlPanel.add(SE);
+
+        S = new JButton("S");
+        S.addActionListener(this);
+        controlPanel.add(S);
+
+        SW = new JButton("SW");
+        SW.addActionListener(this);
+        controlPanel.add(SW);
+
+        W = new JButton("W");
+        W.addActionListener(this);
+        controlPanel.add(W);
+
+        NW = new JButton("NW");
+        NW.addActionListener(this);
+        controlPanel.add(NW);
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+        AppFactory factory = new MineFieldFactory();
+        AppPanel panel = new MineFieldPanel(factory);
+        panel.display();
+    }
+
+}
