391e1fc4dabaedd6437656dc690bb3d5784c6dca
stanley-nguyen <113128619+stanley-nguyen@users.noreply.github.com>
2023-03-20 20:04:45 -0700

    Update AppFactory.java

--- src/mvc/AppFactory.java
+++ src/mvc/AppFactory.java
@@ -4,15 +4,15 @@ package mvc;
 Edits:
     Stanley 3/8/23: created file
     Stanley 3/8/23: completed file
+    Stanley 3/16/23: updated makeView() and makeEditCommand() with new parameters
 */
-
 
 public interface AppFactory {
     public Model makeModel();
-    public View makeView();
+    public View makeView(Model m);
     public String getTitle();
     public String[] getHelp();
     public String about();
     public String[] getEditCommands();
-    public Command makeEditCommand(String s);
+    public Command makeEditCommand(Model m, String s, Object o);
 }
